3c0d65ac736612ecccaeb3f9b591987ce3276f4d
Maya McCann <mccannmaya@gmail.com>
2023-08-01 17:02:27 -0400

    move HTML code in JS and connect with var timeBlockCode

--- script.js
+++ script.js
@@ -16,17 +16,17 @@ if (i < 12) {
 }else{
     var hour_display = (i-12) + "PM"
 }
+timeBlockCode + = <div id="hour-9" class="row time-block past">
+<div class="col-2 col-md-1 hour text-center py-3">9AM</div>
+<textarea class="col-8 col-md-10 description" rows="3"> </textarea>
+<button class="btn saveBtn col-2 col-md-1" aria-label="save">
+  <i class="fas fa-save" aria-hidden="true"></i>
+</button>
+</div>
 }
 }
 }
 
-    <div id="hour-9" class="row time-block past">
-    <div class="col-2 col-md-1 hour text-center py-3">9AM</div>
-    <textarea class="col-8 col-md-10 description" rows="3"> </textarea>
-    <button class="btn saveBtn col-2 col-md-1" aria-label="save">
-      <i class="fas fa-save" aria-hidden="true"></i>
-    </button>
-  </div>
 
     // TODO: Add a listener for click events on the save button. This code should
     // use the id in the containing time-block as a key to save the user input in
